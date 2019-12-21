09b54163f2ec87f066963e04ea20b92f59a72863
masal9pse <returnymgstokh@icloud.com>
2019-12-21 21:51:48 +0900

    インデント調整

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 try {
-  $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;charset=utf8','root','');
-}catch(PDOException $e){
+  $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;charset=utf8', 'root', '');
+} catch (PDOException $e) {
   print('DB接続エラー' . $e->getMessage());
 }
-?>
