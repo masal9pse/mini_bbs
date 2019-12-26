904f83c6c922e822913f5ef69214e64d9c2e6911
masal9pse <returnymgstokh@icloud.com>
2019-12-27 01:13:36 +0900

    79 フロントに表示できたが、投稿できない

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try {
     //dbname= イコール、:ではなく＝
-    $db = new PDO('mysql:dbname=mini_bbs2;host=127.0.0.1;charset=utf8', 'root', '');
+    $db = new PDO('mysql:dbname=mini_bbs;host=127.0.0.1;charset=utf8', 'root', '');
 } catch (PDOException $e) {
     print('DB接続エラー' . $e->getMessage());
 }
